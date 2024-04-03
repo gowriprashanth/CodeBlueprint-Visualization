e83fab8ab8539420c903f9df076da009726c87b6
Gowri Prashanth Kanagraj <gowriprashanth06@gmail.com>
2024-04-03 09:45:35 -0300

    fix

---
+++ test/debounce.js
@@ -0,0 +1,11 @@
+export function debounce(func, delay) {
+    let timer;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(context, args);
+        }, delay);
+    };
+}
